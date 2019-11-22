ebac614cc407b0ebcc6f05029736078f9fa60149
Brian Zelip <bzelip@gmail.com>
2019-11-22 18:03:13 -0500

    main.js: Add if statement around clipboard to check safari

--- main.js
+++ main.js
@@ -53,14 +53,18 @@ import { polygonContains } from 'd3-polygon';
         coordinatesWrapperEl.classList.replace('hide', 'fadein');
 
         coordinatesEL.addEventListener('click', () => {
-          navigator.clipboard.writeText(coordsAsText).then(
-            function() {
-              console.log('Coordinates written to the clipboard!');
-            },
-            function() {
-              console.log('Coordinates failed to write to the clipboard');
-            }
-          );
+          if (navigator.clipboard.writeText) {
+            navigator.clipboard.writeText(coordsAsText).then(
+              function() {
+                console.log('Coordinates written to the clipboard!');
+              },
+              function() {
+                console.log('Coordinates failed to write to the clipboard');
+              }
+            );
+          } else {
+            console.log('NO navigator.clipboard.writeText');
+          }
         });
 
         console.log('position is: ', position);
