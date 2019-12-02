0fcd21c59fa3ec272c14707d871f6d32a6323e47
Brian Zelip <bzelip@gmail.com>
2019-12-02 12:53:39 -0500

    main.js: Refactor around sub modules to write to localStorage 🎉

--- main.js
+++ main.js
@@ -1,50 +1,20 @@
-import { dataImageUrl } from './topography.js';
+import themeHandler from './themeHandler.js';
 import storageAvailable from './storageAvailable.js';
-import geo from './geolocation.js';
+import xhrHandler from './xhrHandler.js';
+import neighborhoodHandler from './neighborhoodHandler.js';
 
 (function() {
-  document.body.style.backgroundImage = dataImageUrl('dark');
-  const buttonEl = document.querySelector('button');
-
-  buttonEl.addEventListener('click', function() {
-    let currentTheme = document.body.className;
-
-    if (currentTheme === 'dark') {
-      document.body.className = 'light';
-      document.body.style.backgroundImage = dataImageUrl('light');
-    } else if (currentTheme === 'light') {
-      document.body.className = 'dark';
-      document.body.style.backgroundImage = dataImageUrl('dark');
+  if (storageAvailable('localStorage')) {
+    if (!localStorage.getItem('NEIGHBORHOODS')) {
+      console.log(
+        'localstorage.NEIGHBORHOODS does not yet exist, but soon should.'
+      );
+      xhrHandler(true);
+    } else {
+      console.log('Awesome, localstorage.NEIGHBORHOODS exists!');
+      neighborhoodHandler();
     }
-  });
-
-  // XHR setup via
-  // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequestEventTarget/onload#Example
-
-  const xhr = new XMLHttpRequest();
-  const method = 'GET';
-  const url = 'https://data.baltimorecity.gov/resource/h3fx-54q3.geojson';
-
-  xhr.open(method, url, true);
-
-  xhr.onload = function() {
-    const NEIGHBORHOODS = JSON.parse(this.responseText);
-
-    const hoodWrapEl = document.querySelector('[data-hood-wrap]');
-    const neighborhoodEl = document.querySelector('[data-neighborhood]');
-    const coordinatesWrapperEl = document.querySelector(
-      '[data-coordinates-wrapper]'
-    );
-    const coordinatesEL = document.createElement('pre');
-    coordinatesEL.classList.add('small');
-    geo({
-      NEIGHBORHOODS,
-      coordinatesEL,
-      hoodWrapEl,
-      neighborhoodEl,
-      coordinatesWrapperEl
-    });
-  };
-
-  xhr.send();
+  } else {
+    xhrHandler(false);
+  }
 })();
