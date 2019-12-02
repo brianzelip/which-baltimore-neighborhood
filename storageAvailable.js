cc89e4ac34c0d0ca4087f4bc442c9522563ddf8c
Brian Zelip <bzelip@gmail.com>
2019-12-02 10:34:35 -0500

    storageAvailable.js: Change var to const

--- storageAvailable.js
+++ storageAvailable.js
@@ -6,7 +6,7 @@ export default function storageAvailable(type) {
   var storage;
   try {
     storage = window[type];
-    var x = '__storage_test__';
+    const x = '__storage_test__';
     storage.setItem(x, x);
     storage.removeItem(x);
     return true;
