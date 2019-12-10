52ec7787ffe2456931f11bb731e311e2d4a9ede3
Brian Zelip <bzelip@gmail.com>
2019-12-10 06:38:19 -0500

    localStorage.js: Start module with isAvaiable()

---
+++ modules/localStorage.js
@@ -0,0 +1,45 @@
+'use-strict';
+
+function isAvailable(type) {
+  // via https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#Testing_for_availability
+
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === 'QuotaExceededError' ||
+        // Firefox
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+  /**
+   * use it like:
+   *
+   * ```js
+   if (storageAvailable('localStorage')) {
+      // Yippee! We can use localStorage awesomeness
+    }
+    else {
+      // Too bad, no localStorage for us
+    }
+   * ```
+   *
+   */
+}
+
+export { isAvailable };
