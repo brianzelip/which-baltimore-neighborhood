abb298db5c1a5aad2a19ee0dc4aa9bfa04082767
Brian Zelip <bzelip@gmail.com>
2019-12-10 09:59:48 -0500

    checkLocalTheme: Set body bgImage on page load

--- modules/checkLocalTheme.js
+++ modules/checkLocalTheme.js
@@ -1,13 +1,17 @@
 'use stric';
 
 import * as ls from './localStorage';
+import { dataImageUrl } from './topography.js';
 
 if (ls.isAvailable()) {
   if (ls.get('THEME')) {
-    const bodyEl = document.querySelector('body');
-    bodyEl.setAttribute('class', ls.get('THEME'));
+    const theme = ls.get('THEME');
+
+    document.body.style.backgroundImage = dataImageUrl(theme);
+    document.body.setAttribute('class', theme);
     console.log('Awesome, localStorage.THEME exists!');
   } else {
+    document.body.style.backgroundImage = dataImageUrl('dark');
     console.log('localStorage.THEME does not yet exist, but soon should.');
   }
 }
